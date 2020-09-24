9be286bd368ddcc547b3346c3de3d88750cb6a89
Dmitriy Esko <Dmitriy.esko@gmail.com>
2020-09-24 10:48:43 +0300

    new react-element

--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
 import React from 'react';
-import './App.css';
-import * as numbers from './numbers';
 
-const a = 5;
-const b = true;
+const newEl = (
+  <div style={{
+    fontFamily: "sans-serif",
+  }}>
+    New Element from App.js<br />
+    Hi there
+  </div>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p style={{
-          color: "grey",
-          fontSize: "2em",
-        }}>
-          count({numbers.count}) * length({numbers.length}) = {numbers.multiply()}
-        </p>
-
-        <p>
-          Variable a = {a}
-        </p>
-
-        <p>
-          Multiplier = {3}
-        </p>
-
-        <p>
-          op. = {a * 3}
-        </p>
-
-        <p>{b && "B = true(logic)"}</p>
-
-        <p>{b ? "B = true(compare)": "B = false(compare)"}</p>
-
-        <p>
-          Other:<br/>
-          1.undefined - {undefined}<br/>
-          2.null - {null}<br/>
-          3.true - {true}<br/>
-          4.false - {false}
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { newEl };
--- src/numbers.js
+++
@@ -1,6 +0,0 @@
-const count = 42;
-const length = 2;
-
-const multiply = () => count * length;
-
-export {count, length, multiply};
