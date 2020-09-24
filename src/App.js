f6dcd38084f411a45dcc81cd2e3eadd339789744
Dmitriy Esko <Dmitriy.esko@gmail.com>
2020-09-24 10:12:02 +0300

    apply changes on App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let a = 5;
+let b = true;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{
+          color: "grey",
+          fontSize: "2em",
+        }}>
+          This string has own style
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>
+          Variable a = {a}
+        </p>
+
+        <p>
+          Multiplier = {3}
+        </p>
+
+        <p>
+          op. = {a * 3}
+        </p>
+
+        <p>{b && "B = true(logic)"}</p>
+
+        <p>{b ? "B = true(compare)": "B = false(compare)"}</p>
+
+        <p>
+          Other:<br/>
+          1.undefined - {undefined}<br/>
+          2.null - {null}<br/>
+          3.true - {true}<br/>
+          4.false - {false}
+        </p>
       </header>
     </div>
   );
