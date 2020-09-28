e391d17a6bf37b817b16b53b6ab4b9da05007a1e
Dmitriy Esko <Dmitriy.esko@gmail.com>
2020-09-28 22:23:23 +0300

    Props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import './App.css';
 
+const tasks = ['Разобраться с props-ми', 'Впилить их в проект', 'Отпрвить его на проверку']
+
 const App = () => (
   <div>
     <h1>Todo list</h1>
     <InputItem />
-    <ItemList />
-    <Footer />
+    <ItemList tasks={tasks}/>
+    <Footer tasksCounter={tasks.length}/>
   </div>
 );
 
