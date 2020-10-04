eac079c7f3f013ff63f54014bffcec117ecb08a5
Dmitriy Esko <Dmitriy.esko@gmail.com>
2020-10-04 21:33:03 +0300

    class-component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,35 +4,36 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const tasks = [
-  {
-    value: 'Разобраться с props-ми',
-    isDone: true,
-  },
-  {
-    value: 'Впилить их в проект',
-    isDone: true,
-  },
-  {
-    value: 'Отпрвить его на проверку',
-    isDone: true,
-  },
-  {
-    value: 'Закончить реализацию проекта',
-    isDone: false,
+class App extends React.Component {
+  render() {
+    const tasks = [
+      {
+        value: 'Разобраться с props-ми',
+        isDone: true,
+      },
+      {
+        value: 'Впилить их в проект',
+        isDone: true,
+      },
+      {
+        value: 'Отпрвить его на проверку',
+        isDone: true,
+      },
+      {
+        value: 'Закончить реализацию проекта',
+        isDone: false,
+      }
+    ];
+    
+    return (
+      <div className={styles.wrapper}>
+        <h1>Todo list</h1>
+        <InputItem />
+        <ItemList tasks={tasks} />
+        <Footer tasksCounter={tasks.length}/>
+      </div>
+    )
   }
-]
-
-// Счетчик, в котором отображается количество дел, которые еще не сделаны
-// Кнопка  "Удалить выполненные дела" 
-
-const App = () => (
-  <div className={styles.wrapper}>
-    <h1>Todo list</h1>
-    <InputItem />
-    <ItemList tasks={tasks} />
-    <Footer tasksCounter={tasks.length}/>
-  </div>
-);
+}
 
 export default App;
