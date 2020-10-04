36e707a67ffc10847fc82c27ac9ec6e2e5cfb9ae
Dmitriy Esko <Dmitriy.esko@gmail.com>
2020-10-04 22:18:15 +0300

    events

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Item from '../Item/Item';
 import styles from'./ItemList.module.css';
 
-const ItemList = ({tasks}) => ( <ul className={styles.itemList}>
-  {tasks.map( (task) => <Item key={task.value} value={task.value} isDone={task.isDone} />)}
+const ItemList = ({tasks, onClickDone}) => ( <ul className={styles.itemList}>
+  {tasks.map( (task) => <Item key={task.value} value={task.value} isDone={task.isDone} onClickDone={onClickDone}/>)}
   </ul>
 );
 
