185dba4b51f18d4b333daf78780355b59b906f30
Dmitriy Esko <Dmitriy.esko@gmail.com>
2020-10-13 21:32:00 +0300

    Messages check

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Contacts = () => {
   return(
-    <p>My Contacts</p>
+    <div>
+      <p>My Contacts</p>
+      <a href="https://tele.gs/Aug512">Telegram</a>
+      <br />
+      <a href="https://vk.com/write36437279">VK</a>
+      <br />
+      <a href="https://wa.me/79650157820">WhatsApp</a>
+    </div>
   )
 }
 
